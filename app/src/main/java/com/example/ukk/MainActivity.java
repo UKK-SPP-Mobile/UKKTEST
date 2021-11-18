411807c6eef809cd666ab930fbf6aabdf055e031
delxcign <57690820+delxcign@users.noreply.github.com>
2021-11-18 15:41:42 +0700

    c1 wahyu

---
+++ app/src/main/java/com/example/ukk/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ukk;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    //test commmit no1
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
